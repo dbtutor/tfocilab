452178e5cc276ebaecfba1f7458f1291db4b97e7
Nayan <nayanmailbox@gmail.com>
2020-04-26 17:14:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,3 @@
-
-/*
-module "admin" {
-  source             = "./modules/admin" 
-  enable_admin       = var.enable_admin
-  lab_name           = var.lab_name
-  students           = var.students
-  tenancy_ocid       = var.tenancy_ocid
-  top_compartment_id = var.top_compartment_id  
-  
-  providers          = {
-    oci      = oci
-    oci.home = oci.home
-  }
-}
-*/
-
 module "indentity_compartment" {
   source                = "./modules/identity/compartment"
   #is_create_resource    = var.is_create_resource
@@ -31,19 +14,3 @@ module "indentity_compartment_child" {
   compartment_name		  = var.compartment_name_child
   compartment_descr		  = var.compartment_descr_child
 }
-
-
-
-
-
-/*
-module "networking" {
-  source              = "./modules/networking"  
-  enable_network      = var.enable_network
-  lab_name            = var.lab_name
-  students            = var.students
-  # tenancy_ocid        = var.tenancy_ocid
-  top_compartment_id  = var.top_compartment_id  
-  lab_compartment_ids = module.admin.lab_compartment_ids  
-}
-*/
