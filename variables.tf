a0a4068b8e524523556c2904f7ee6735bcabaf93
Nayan <nayanmailbox@gmail.com>
2020-04-26 19:35:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
-variable "private_key_path" {}
+variable "private_key" {}
 variable "region" {}
 variable "compartment_ocid" {}
 variable "ssh_public_key" {}
