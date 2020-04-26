25e7a327f76c0e120e6604732d34fa7f93ced4e5
Nayan <nayanmailbox@gmail.com>
2020-04-26 20:14:21 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,3 @@ output "compartment_parent" {
 output "compartment_child" {
   value = module.indentity_compartment_child
 }
-
-/*
-output "instance_psft_elk_ip" {
-  value = module.instance-elk.public_ip
-}
-*/
