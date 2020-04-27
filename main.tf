8b14a27a45f188abddb290b6155912c3d50df026
Nayan <nayanmailbox@gmail.com>
2020-04-27 18:28:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,26 @@
+# https://github.com/cloudposse/terraform-null-label#simple-example
+module "null_label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
+  # namespace  = "eg"
+  # stage      = "prod"
+  # name       = "bastion"
+  # attributes = ["public"]
+  # delimiter  = "-"
+
+  tags = {
+    "BusinessUnit" = "XYZ",
+    "Snapshot"     = "true"
+  }
+}
+
 module "indentity_compartment" {
   source                = "./modules/identity/compartment"
   #is_create_resource    = var.is_create_resource
   compartment_ocid	    = var.compartment_ocid
   compartment_name		  = var.compartment_name
   compartment_descr		  = var.compartment_descr
+  
+  freeform_tags         = module.null_label.tags
 }
 
 # Creating Child Compartment
@@ -13,4 +30,6 @@ module "indentity_compartment_child" {
   compartment_ocid	    = module.indentity_compartment.CompartmentNameOutputs
   compartment_name		  = var.compartment_name_child
   compartment_descr		  = var.compartment_descr_child
+    
+  freeform_tags         = module.null_label.tags
 }
