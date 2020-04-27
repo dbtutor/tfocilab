6adcecd301bda37a7c46c31c375646bddb415efe
Nayan <nayanmailbox@gmail.com>
2020-04-27 18:36:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,21 +15,18 @@ module "null_label" {
 
 module "indentity_compartment" {
   source                = "./modules/identity/compartment"
-  #is_create_resource    = var.is_create_resource
+  #is_create_resource   = var.is_create_resource
   compartment_ocid	    = var.compartment_ocid
   compartment_name		  = var.compartment_name
   compartment_descr		  = var.compartment_descr
-  
   freeform_tags         = module.null_label.tags
 }
 
 # Creating Child Compartment
 module "indentity_compartment_child" {
   source                = "./modules/identity/compartment"
-  #is_create_resource    = var.is_create_resource
   compartment_ocid	    = module.indentity_compartment.CompartmentNameOutputs
   compartment_name		  = var.compartment_name_child
   compartment_descr		  = var.compartment_descr_child
-    
   freeform_tags         = module.null_label.tags
 }
